e5df3ed80d39cad7239d7d8f44f049b302459391
Andreas Pålsson <apals@kth.se>
2014-11-03 19:29:28 -0500

    add texture mapping. sphere disappeared though. no clue why

--- project4/project4.js
+++ project4/project4.js
@@ -25,10 +25,7 @@ var projection;
 var modelView;
 var aspect;
 
-
-var ambientColor;
-
-var pointsArray = [];
+var ctm;
 
 var index = 0;
 
@@ -39,6 +36,54 @@ var vPosition;
 var program;
 var lpostBuffer;
 var lpostBuffer2;
+var buffer;
+var sampler;
+
+var tBuffer;
+var vTexCoord;
+
+var cBuffer;
+var vColor;
+	
+var numVertices  = 36;
+
+var pointsArray = [];
+var colorsArray = [];
+var texCoordsArray = [];
+
+var texSize = 64;
+var numRows = 8;
+var numCols = 8;
+var myTexels = new Uint8Array(4*texSize*texSize);
+
+var vertices = [
+	   vec4(0.0, 0.0-10, cubeSize, 1.0),
+	   vec4(0.0, cubeSize-10, cubeSize, 1.0),
+	   vec4(cubeSize, cubeSize-10, cubeSize, 1.0),
+	   vec4(cubeSize, 0.0-10, cubeSize, 1.0),
+	   vec4(0.0, 0.0-10, 0.0-1000, 1.0),
+	   vec4(0.0, cubeSize-10, 0.0-1000, 1.0),
+	   vec4(cubeSize, cubeSize-10, 0.0-1000, 1.0),
+	   vec4(cubeSize, 0.0-10, 0.0-1000, 1.0)
+	];
+
+var vertexColors = [
+        vec4( 0.0, 0.0, 0.0, 1.0 ),  // black
+        vec4( 1.0, 0.0, 0.0, 1.0 ),  // red
+        vec4( 1.0, 1.0, 0.0, 1.0 ),  // yellow
+        vec4( 0.0, 1.0, 0.0, 1.0 ),  // green
+        vec4( 0.0, 0.0, 1.0, 1.0 ),  // blue
+        vec4( 1.0, 0.0, 1.0, 1.0 ),  // magenta
+        vec4( 0.0, 1.0, 1.0, 1.0 ),  // cyan
+        vec4( 1.0, 1.0, 1.0, 1.0 ),  // white
+    ];
+
+var texCoord = [
+		vec2(0,0),
+		vec2(0,2),
+		vec2(2,2),
+		vec2(2,0)
+];
 
 function triangle(a, b, c) {
      pointsArray.push(a); 
@@ -71,6 +116,47 @@ function tetrahedron(a, b, c, d, n) {
     divideTriangle(a, c, d, n);
 }
 
+var roadTexture;
+var roadImage;
+
+function quad(a, b, c, d) {
+     //pointsArray.push(vertices[a]); 
+     colorsArray.push(vertexColors[a]); 
+	 texCoordsArray.push(texCoord[0]);
+	 
+     //pointsArray.push(vertices[b]); 
+     colorsArray.push(vertexColors[a]); 
+	 texCoordsArray.push(texCoord[1]);
+	 
+     //pointsArray.push(vertices[c]); 
+     colorsArray.push(vertexColors[a]);    
+	 texCoordsArray.push(texCoord[2]);
+	 
+     //pointsArray.push(vertices[a]); 
+     colorsArray.push(vertexColors[a]); 
+	 texCoordsArray.push(texCoord[0]);
+
+     //pointsArray.push(vertices[c]); 
+     colorsArray.push(vertexColors[a]); 
+	 texCoordsArray.push(texCoord[2]);
+
+     //pointsArray.push(vertices[d]); 
+     colorsArray.push(vertexColors[a]); 
+	 texCoordsArray.push(texCoord[3]);
+}
+
+// Each face determines two triangles
+
+function colorCube()
+{
+    quad( 1, 0, 3, 2 );
+    quad( 2, 3, 7, 6 );
+    quad( 3, 0, 4, 7 );
+    quad( 6, 5, 1, 2 );
+    quad( 4, 5, 6, 7 );
+    quad( 5, 4, 0, 1 );
+}
+
 window.onload = function init()
 {
     canvas = document.getElementById( "gl-canvas" );
@@ -79,44 +165,11 @@ window.onload = function init()
     if ( !gl ) { alert( "WebGL isn't available" ); }
 
 	// Load vertices and colors for cube faces
+	colorCube();
 	
-	vertices = [
-	   vec4(0.0, 0.0-10, cubeSize, 1.0),
-	   vec4(0.0, cubeSize-10, cubeSize, 1.0),
-	   vec4(cubeSize, cubeSize-10, cubeSize, 1.0),
-	   vec4(cubeSize, 0.0-10, cubeSize, 1.0),
-	   vec4(0.0, 0.0-10, 0.0-1000, 1.0),
-	   vec4(0.0, cubeSize-10, 0.0-1000, 1.0),
-	   vec4(cubeSize, cubeSize-10, 0.0-1000, 1.0),
-	   vec4(cubeSize, 0.0-10, 0.0-1000, 1.0)
-	];
 	
-	var lampPostHeight = 10;
-	var lampPostWidth = 1;
-	var lampPostDepth = 1;
 	
-	vertices2 = [
-	   vec4(0.0	, 0.0	, 0, 1.0),
-	   vec4(0.0, lampPostHeight, 0, 1.0),
-	   vec4(lampPostWidth, lampPostHeight, 0, 1.0),
-	   vec4(lampPostWidth, 0, 0, 1.0),
-	   vec4(0.0, 0, -lampPostDepth, 1.0),
-	   vec4(0.0, lampPostHeight, -lampPostDepth, 1.0),
-	   vec4(lampPostWidth, lampPostHeight, -lampPostDepth, 1.0),
-	   vec4(lampPostWidth, 0, -lampPostDepth, 1.0)
-	];
-	
-	vertices3 = [	   
-	   //the upper end of the light post
-	   vec4(-2*lampPostWidth, lampPostHeight, 0, 1.0),
-	   vec4(-2*lampPostWidth, lampPostHeight+1, 0, 1.0),
-	   vec4(lampPostWidth, lampPostHeight+1, 0, 1.0),
-	   vec4(lampPostWidth, lampPostHeight, 0, 1.0),
-	   vec4(-2*lampPostWidth, lampPostHeight, -lampPostDepth, 1.0),
-	   vec4(-2*lampPostWidth, lampPostHeight+0.1, -lampPostDepth, 1.0),
-	   vec4(lampPostWidth, lampPostHeight+1, -lampPostDepth, 1.0), 
-	   vec4(lampPostWidth, lampPostHeight, -lampPostDepth, 1.0)
-	];
+	setupLightPostVertices();
 	
 	 colors = [
 	    vec4(1.0, 0.0, 0.0, 1.0),  // red
@@ -136,25 +189,12 @@ window.onload = function init()
 	   4, 5, 6, 6, 7, 4,  // back face
 	   5, 4, 0, 0, 1, 5   // left face
 	];
-	
-	//gl.uniform4fv(gl.getUniformLocation(program, "ambientColor"), vec4(1, 1, 0, 1));
-	
-	
-	
-    
+	    
     //  Configure WebGL
     gl.viewport( 0, 0, canvas.width, canvas.height );
 	aspect = canvas.width / canvas.height;
     gl.clearColor( 0.2, 0.2, 0.7, 1.0 );
 	gl.enable(gl.DEPTH_TEST);
-	//projection = ortho (windowMin, windowMax, windowMin, windowMax, windowMin, windowMax+cubeSize);
-	// Register event listeners for the buttons
-	
-	
-	/*var va = vec4(0.0, 0.0, -1.0, 1);
-    var vb = vec4(0.0, 0.942809, 0.333333, 1);
-    var vc = vec4(-0.816497, -0.471405, 0.333333, 1);
-    var vd = vec4(0.816497, -0.471405, 0.333333, 1);*/
 	
 	var va = vec4(0.0, 0.0, -1.0, 1);
     var vb = vec4(0.0, 0.942809, 0.333333, 1);
@@ -170,59 +210,78 @@ window.onload = function init()
     gl.useProgram( program );
 	
 	
-	var lightAmbient = vec4(0, 1, 0, 1.0 );
-	var materialAmbient = vec4( 1.0, 0.0, 1.0, 1.0 );
-	ambientProduct = mult(lightAmbient, materialAmbient);
-	console.log(ambientProduct + " :: AA ");
-	var ambientProductPlace = gl.getUniformLocation(program, 
-       "ambientProduct");
-	console.log("BBB :: " + ambientProductPlace);
-	gl.uniform4fv(ambientProductPlace, flatten(ambientProduct));
+	for (var i=0; i<texSize; i++) {
+	   for (var j=0; j<texSize; j++) {
+	      var patchx = Math.floor(i/(texSize/numRows));
+		  var patchy = Math.floor(j/(texSize/numCols));
+		  var c = (patchx%2 !== patchy%2 ? 255 : 0);
+		  myTexels[4*i*texSize+4*j] = c;
+		  myTexels[4*i*texSize+4*j+1] = c;
+		  myTexels[4*i*texSize+4*j+2] = c;
+		  myTexels[4*i*texSize+4*j+3] = 255;
+		}
+	}
+	
+	var texture = gl.createTexture();
+	gl.bindTexture (gl.TEXTURE_2D, texture);
+	gl.texImage2D (gl.TEXTURE_2D, 0, gl.RGBA, texSize, texSize, 0, gl.RGBA, gl.UNSIGNED_BYTE, myTexels);
+	//gl.texParameteri (gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+	//gl.texParameteri (gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+	//gl.texParameteri (gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.MIRRORED_REPEAT);
+	//gl.texParameteri (gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.MIRRORED_REPEAT);
+ 	gl.texParameteri (gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+ 	//gl.texParameteri (gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+	//gl.generateMipmap( gl.TEXTURE_2D );
+	gl.uniform1i(gl.getUniformLocation(program, "texMap"), 0);
+	
+
     
+	sampler = gl.getUniformLocation(program, "uSampler");
 	colorLoc = gl.getUniformLocation (program, "color");
 	modelViewLoc = gl.getUniformLocation (program, "modelView");
 	projectionLoc  = gl.getUniformLocation (program, "projection");
 	
-    vBuffer = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
 	
-	lpostBuffer = gl.createBuffer();
-	gl.bindBuffer( gl.ARRAY_BUFFER, lpostBuffer);
-	gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices2), gl.STATIC_DRAW );
+	initSphereBuffer();
+	initIndexBuffer();
+	initLightPostBuffers();
 	
-	lpostBuffer2 = gl.createBuffer();
-	gl.bindBuffer( gl.ARRAY_BUFFER, lpostBuffer2);
-	gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices3), gl.STATIC_DRAW );
+	initColorBuffer();
+    	
+    vColor = gl.getAttribLocation( program, "vColor" );
+	gl.vertexAttribPointer( vColor, 4, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vColor);
+	
+    initRoadBuffer();
 
+	
+	tBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, tBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, flatten(texCoordsArray), gl.STATIC_DRAW);
+	
+	vTexCoord = gl.getAttribLocation (program, "vTexCoord");
+	gl.vertexAttribPointer (vTexCoord, 2, gl.FLOAT, false, 0, 0);
+	gl.enableVertexAttribArray(vTexCoord);
+		
     vPosition = gl.getAttribLocation( program, "vPosition" );
     gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( vPosition );
 	
-	iBuffer = gl.createBuffer();
-	gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, iBuffer);
-	gl.bufferData( gl.ELEMENT_ARRAY_BUFFER, new Uint8Array(indices), gl.STATIC_DRAW);
 	
-	
-	sphereBuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, sphereBuffer);
-	gl.bufferData(gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW);
-	//gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0 );
 	
     render();
 };
-
 function render()
 {
     gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-	//theta[axis] += 0.5;
 
-	drawRoad();
 	
+	drawRoad();
+	drawSphere(); 
 	drawLampPost();
 	
 	drawLampPostTop();	
-	drawSphere(); 
+	
 	   
 	requestAnimFrame (render);
 };
@@ -230,7 +289,6 @@ function render()
 function drawRoad() {
 	gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
 	gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0 );
-	
 	
 	//USE THIS FOR TRANSLATION 
 	tz1 = mat4 (1.0, 0.0, 0.0, -cubeSize2,
@@ -248,10 +306,6 @@ function drawRoad() {
 	modelView = mult(looking, mult(tz2, tz1));
 	gl.uniformMatrix4fv (modelViewLoc, false, flatten(modelView));
 	gl.uniformMatrix4fv (projectionLoc, false, flatten(projection));
-	translation = [0, 0, 0, 0];
-	scale = [1, 1, 1, 1];
-	gl.uniform4fv(gl.getUniformLocation(program, "translation"), flatten(translation));
-	gl.uniform4fv(gl.getUniformLocation(program, "scale"), flatten(scale));
 	for (var i=0; i<6; i++) {
 		gl.uniform4fv (colorLoc, colors[i]);
 		gl.drawElements( gl.TRIANGLES, 6, gl.UNSIGNED_BYTE, 6*i );
@@ -279,10 +333,6 @@ function drawLampPost() {
 	modelView = mult(looking, mult(tz2, tz1));
 	gl.uniformMatrix4fv (modelViewLoc, false, flatten(modelView));
 	gl.uniformMatrix4fv (projectionLoc, false, flatten(projection));
-	translation = [0, 0, 0, 0];
-	scale = [1, 1, 1, 1];
-	gl.uniform4fv(gl.getUniformLocation(program, "translation"), flatten(translation));
-	gl.uniform4fv(gl.getUniformLocation(program, "scale"), flatten(scale));
 	for (var i=0; i<6; i++) {
 		gl.uniform4fv (colorLoc, colors[i]);
 		gl.drawElements( gl.TRIANGLES, 6, gl.UNSIGNED_BYTE, 6*i );
@@ -310,10 +360,6 @@ function drawLampPostTop() {
 	modelView = mult(looking, mult(tz2, tz1));
 	gl.uniformMatrix4fv (modelViewLoc, false, flatten(modelView));
 	gl.uniformMatrix4fv (projectionLoc, false, flatten(projection));
-	translation = [0, 0, 0, 0];
-	scale = [1, 1, 1, 1];
-	gl.uniform4fv(gl.getUniformLocation(program, "translation"), flatten(translation));
-	gl.uniform4fv(gl.getUniformLocation(program, "scale"), flatten(scale));
 	for (var i=0; i<6; i++) {
 		gl.uniform4fv (colorLoc, colors[i]);
 		gl.drawElements( gl.TRIANGLES, 6, gl.UNSIGNED_BYTE, 6*i );
@@ -322,13 +368,12 @@ function drawLampPostTop() {
 function drawSphere() {
 
 	gl.bindBuffer( gl.ARRAY_BUFFER, sphereBuffer );
-	gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0 );
-	
+	gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0 );	
 	
 	//USE THIS FOR TRANSLATION 
-	tz1 = mat4 (1.0, 0.0, 0.0, -cubeSize2,
-			   0.0, 1.0, 0.0, -cubeSize2,
-			   0.0, 0.0, 1.0, -cubeSize2+20,
+	tz1 = mat4 (1.0, 0.0, 0.0, -cubeSize2+5,
+			   0.0, 1.0, 0.0, -cubeSize2+5,
+			   0.0, 0.0, 1.0, -cubeSize2-10,
 			   0.0, 0.0, 0.0, 1.0);
 			   
 	tz2 = mat4 (1.0, 0.0, 0.0, cubeSize2,
@@ -342,13 +387,77 @@ function drawSphere() {
 	gl.uniformMatrix4fv (modelViewLoc, false, flatten(modelView));
 	gl.uniformMatrix4fv (projectionLoc, false, flatten(projection));
 	
-	translation = [12, 12, 2.99, 1];
-	gl.uniform4fv(gl.getUniformLocation(program, "translation"), flatten(translation));
-	scale = [1, 1, 1, 1];
-	gl.uniform4fv(gl.getUniformLocation(program, "scale"), flatten(scale));
 	
-	gl.uniform4fv(colorLoc, vec4(0.99, 0.72, 0.075, 1));
-	for( var i=0; i<index; i+=3) 
+	
+	for( var i=0; i<index; i+=3)  {
+		gl.uniform4fv(colorLoc, vec4(0.99, 0.72, 0.075, 1));
        gl.drawArrays( gl.LINE_LOOP, i, 3 );
-	  
+	}	  
+}
+
+function initIndexBuffer() {
+	iBuffer = gl.createBuffer();
+	gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, iBuffer);
+	gl.bufferData( gl.ELEMENT_ARRAY_BUFFER, new Uint8Array(indices), gl.STATIC_DRAW);
+	
+}
+function initLightPostBuffers() {
+	
+	lpostBuffer = gl.createBuffer();
+	gl.bindBuffer( gl.ARRAY_BUFFER, lpostBuffer);
+	gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices2), gl.STATIC_DRAW );
+	
+	lpostBuffer2 = gl.createBuffer();
+	gl.bindBuffer( gl.ARRAY_BUFFER, lpostBuffer2);
+	gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices3), gl.STATIC_DRAW );
+	
+}
+
+function initRoadBuffer() {
+	vBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
+}
+
+function initSphereBuffer() {
+	sphereBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, sphereBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW);
+}
+
+function initColorBuffer() {
+	
+	cBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(colorsArray), gl.STATIC_DRAW );
+}
+
+
+function setupLightPostVertices() {
+
+	var lampPostHeight = 10;
+	var lampPostWidth = 1;
+	var lampPostDepth = 1;
+	vertices2 = [
+	   vec4(0.0	, 0.0	, 0, 1.0),
+	   vec4(0.0, lampPostHeight, 0, 1.0),
+	   vec4(lampPostWidth, lampPostHeight, 0, 1.0),
+	   vec4(lampPostWidth, 0, 0, 1.0),
+	   vec4(0.0, 0, -lampPostDepth, 1.0),
+	   vec4(0.0, lampPostHeight, -lampPostDepth, 1.0),
+	   vec4(lampPostWidth, lampPostHeight, -lampPostDepth, 1.0),
+	   vec4(lampPostWidth, 0, -lampPostDepth, 1.0)
+	];
+	
+	vertices3 = [	   
+	   //the upper end of the light post
+	   vec4(-2*lampPostWidth, lampPostHeight, 0, 1.0),
+	   vec4(-2*lampPostWidth, lampPostHeight+1, 0, 1.0),
+	   vec4(lampPostWidth, lampPostHeight+1, 0, 1.0),
+	   vec4(lampPostWidth, lampPostHeight, 0, 1.0),
+	   vec4(-2*lampPostWidth, lampPostHeight, -lampPostDepth, 1.0),
+	   vec4(-2*lampPostWidth, lampPostHeight+0.1, -lampPostDepth, 1.0),
+	   vec4(lampPostWidth, lampPostHeight+1, -lampPostDepth, 1.0), 
+	   vec4(lampPostWidth, lampPostHeight, -lampPostDepth, 1.0)
+	];
 }
